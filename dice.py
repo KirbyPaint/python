74aae9e56613862e6a88d5c28cf2c63e0c72a461
KirbyPaint <porter.ashley13@gmail.com>
2023-12-04 18:15:12 -0800

    pass argument to roll n dice, should be between 3 and 5 for our rules)

--- dice.py
+++ dice.py
@@ -1,25 +1,41 @@
 # dice.py
 
+import sys
 import random
 
+# Rolls a number of dice for one stat (rolls 3, 4, or 5)
 def roll_dice(num_dice):
+    # Array to hold each die roll
     roll_results = []
+    # The lowest a die can be (in cases of re-rolling 1s and 2s)
+    base = 1
+    match num_dice:
+        case 3:
+            base = 3
+        case 4:
+            base = 2
+        case _:
+            pass # Figure out Python's "break" statement later
+    # Roll the entered quantity
     for _ in range(num_dice):
-        roll = random.randint(2, 6)
+        roll = random.randint(base, 6)
         roll_results.append(roll)
-    return roll_results
+    roll_results.sort(reverse=True)
+    return roll_results[:3]
 
+# Calculate average of a list of integers
 def avg(list):
     return sum(list) / len(list)
 
-def roll_group(num_groups):
+# Rolls a full set of n stats, dropping the lowest
+def roll_group(num_dice):
+    # Array to hold the full group
     stats = []
-    for _ in range(num_groups):
-        roll = roll_dice(4)
-        roll.remove(min(roll))
+    for _ in range(7):
+        roll = roll_dice(num_dice)
         stats.append(sum(roll))
     stats.remove(min(stats))
     print(stats)
     print(avg(stats))
 
-roll_group(7)
+roll_group(int(sys.argv[1]))
